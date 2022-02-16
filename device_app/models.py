ab9d4c1dfa8d28120fad1b3914390b94c93ca732
JakeZ34 <JzBaseball34@hotmail.com>
2022-02-16 15:03:25 -0800

    views/models raw

--- device_app/models.py
+++ device_app/models.py
@@ -1,3 +1,77 @@
 from django.db import models
 
 # Create your models here.
+'''from django.db import models, migrations
+import bcrypt
+import re
+from datetime import datetime
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+
+class UserManager(models.Manager):
+    def validate(self, postData):
+        errors = {}
+        if len(postData['first_name']) < 2:
+            errors['first_name'] = 'First Name must be at least 2 characters'
+        
+        if len(postData['last_name']) < 2:
+            errors['last_name'] = 'Last Name must be at least 2 characters'
+        
+        if not EMAIL_REGEX.match(postData['email']):
+            errors['email'] = 'Invalid Email Address'
+        
+        email_check = self.filter(email=postData['email'])
+        if email_check:
+            errors['email'] = "Already in use"
+        if not email_checker.match(postdata['email']):
+            errors['email'] = 'Email must be valid'
+        if len(postData['password']) < 8:
+            errors['password'] = 'Password must be at least 8 characters'
+        if postData['password'] != postData['confirm']:
+            errors['password'] = 'Passwords do not match'
+        return errors
+    def authenticate(self, email, password):
+        users = self.filter(email=email)
+        if not users:
+            return False
+        user=users[0]
+        return bcrypt.checkpw(password.encode(), user.password.encode())
+    def register(self, postData):
+        pw = bcrypt.hashpw(postData['password'].encode(), bcrypt.gensalt()).decode()
+        return self.create(
+            first_name = postData['first_name'],
+            last_name = postData['last_name'],
+            email = postData['email'],
+            password = pw,
+        )
+        return errors
+
+class User(models.Model):
+    #first_name = models.CharField(max_length=55)
+    #ast_name = models.CharField(max_length=55)
+    #email = models.EmailField(unique=True)
+    #password = models.CharField(max_length=255)
+    #objects = UserManager()
+
+class PostManager(models.Manager):
+    def post_validator(self, postData):
+        errors = {}
+        if len(postData['title']) < 1:
+            errors['title'] = "Post must have a title"
+        if len(postData['description']) < 1:
+            errors['description'] = "Post must have a description"
+        if len(postData['mess']) < 1:
+            errors['mess'] = "Link must not be empty"
+        return errors
+
+class Post(models.Model):
+    message = models.URLField(max_length=200)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    #image = models.ImageField()
+    poster = models.ForeignKey(User, related_name='user_messages', on_delete=models.CASCADE)
+    user_likes = models.ManyToManyField(User, related_name='liked_posts')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = PostManager() 
+#'''
--- device_app/views.py
+++ device_app/views.py
@@ -3,3 +3,148 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+'''from django.shortcuts import render, redirect
+from .models import *
+import bcrypt
+import re
+from django.contrib import messages
+#from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def register(request):
+    if request.method == "GET":
+        return redirect('/')
+    errors = User.objects.validate(request.POST)
+    if errors:
+        for e in errors.values():
+            messages.error(request, e)
+        return redirect('/')
+    else:
+        new_user = User.objects.register(request.POST)
+        request.session['user_id'] = new_user.id
+        return redirect('/main')
+
+
+def login(request):
+    if request.method == "GET":
+        return redirect('/')
+    if not User.objects.authenticate(request.POST['email'], request.POST['password']):
+        messages.error(request, 'Invalid Email/Password')
+        return redirect('/')
+    user = User.objects.get(email=request.POST['email'])
+    request.session['user_id'] = user.id
+    return redirect('/main')
+
+
+#def profile(request):
+    #user = User.objects.get(id=request.session['user_id'])
+    #context = {
+    #"user": User.objects.get(id=request.session['user_id'])
+    #}
+    #return render(request, "profile.html", context)
+
+
+def main_page(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'user': user,
+        'posts': Post.objects.all()
+    }
+    return render(request, 'main.html', context)
+
+
+def make_post(request):
+    if(request.method == "POST"):
+        errors = Post.objects.post_validator(request.POST)
+        if len(errors):
+            for key, value in errors.items():
+                messages.error(request, value)
+            return redirect('/main')
+        else:
+            post = Post.objects.create(
+                message=request.POST['mess'],
+                title=request.POST['title'],
+                description=request.POST['description'],
+                poster=User.objects.get(id=request.session['user_id'])
+            )
+    return redirect('/main')
+
+
+def add_like(request, id):
+    liked_message = Post.objects.get(id=id)
+    user_liking = User.objects.get(id=request.session['user_id'])
+    liked_message.user_likes.add(user_liking)
+    return redirect('/main')
+
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
+
+#@login_required
+
+
+def edit(request, id):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    context = {
+        'posts': Post.objects.filter(id=id)
+    }
+    return render(request, 'updatepost.html', context)
+
+
+def update(request, id):
+    errors = Post.objects.post_validator(request.POST)
+    if len(errors):
+        for key, value in errors.items():
+            messages.error(request, value)
+        return render(request, 'updatepost.html')
+    else:
+        post = Post.objects.get(id=id)
+        post.message = request.POST['mess']
+        post.title = request.POST['title']
+        post.description = request.POST['description']
+        poster = User.objects.get(id=request.session['user_id'])
+        post.save()
+    return redirect('/main')
+
+
+def delete(request, id):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    else:
+        post = Post.objects.get(id=id)
+        post.delete()
+        return redirect('/main')
+
+
+def back(request):
+    return redirect('/main')
+
+
+def show_post(request, id):
+    id = id
+    user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'posts': Post.objects.filter(id=id)
+    }
+    return render(request, 'showpost.html', context)
+
+
+def welcome(request):
+    return render(request, 'loginpage.html')
+
+
+#def show_post(request, id):
+    #context = {
+        #'post': Post.objects.get(id=id)
+    #}
+    #return render(request, 'showpost.html', context)
+'''
